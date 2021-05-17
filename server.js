9fbe825bf48dec620f2fca65d121fedeb96e2612
Don Stephan <donald.m.stephan@gmail.com>
2021-05-17 13:44:24 -0400

    added disconnections and reconnections

--- server.js
+++ server.js
@@ -3,40 +3,43 @@ import { Mongo } from 'meteor/mongo';
 
 import TenantCollection from './collection';
 
-let tenantList = {};
-let driverPool = {};
-let connectionOptions = null;
-
 export const Tenants = {
-  set: function (tenants, disableAutoPublish, remoteConnectionOptions) {
+  TENANTS_POOL_DISCONNECT_TIME: process.env.TENANTS_POOL_DISCONNECT_TIME || 1000 * 10,
+  TENANTS_CLEANUP_INTERVAL: process.env.TENANTS_CLEANUP_INTERVAL || 1000 * 10,
+  collection: TenantCollection,
+  _driverPool: {},
+  _tenantList: [],
+  _connectionOptions: null,
+  set: (tenants, disableAutoPublish, remoteConnectionOptions) => {
     check(tenants, Object);
-    Object.keys(tenants).forEach(function (key) {
-      if (!driverPool[key]) {
-        driverPool[key] = {
+    
+    Object.keys(tenants).forEach((key) => {
+      if (!Tenants._driverPool[key]) {
+        Tenants._driverPool[key] = {
           driver: null,
           lastAccessed: Date.now(),
           connected: false,
           connectionString: tenants[key],
-          collections: []
+          collections: [],
+          collectionNames: []
         };
       }
     });
 
     if (remoteConnectionOptions) {
       check(remoteConnectionOptions, Object);
-      connectionOptions = remoteConnectionOptions;
+      Tenants._connectionOptions = remoteConnectionOptions;
     }
 
     if (!disableAutoPublish) {
-      Meteor.publish(null, function() {
+      Meteor.publish(null, () => {
         return TenantCollection.find({})
       });
     }
   },
-  get: function () {
-    return ["", ...Object.keys(driverPool)];
-  },
-  collection: TenantCollection
+  get: () => {
+    return ["", ...Object.keys(Tenants._driverPool)];
+  }
 }
 
 Mongo.Collection.prototype.from = function (key) {
@@ -49,7 +52,7 @@ Mongo.Collection.prototype.from = function (key) {
     context._tenants = {};
   }
 
-  let tenant = tenantList[key];
+  let tenant = Tenants._tenantList[key];
   if (!tenant) {
     tenant = TenantCollection.findOne({ name: key });
     if (!tenant) {
@@ -63,57 +66,75 @@ Mongo.Collection.prototype.from = function (key) {
       TenantCollection.insert(tenant);
     }
 
-    tenantList[key] = tenant;
+    Tenants._tenantList[key] = tenant;
   }
 
-  if (!driverPool[key].connected) {
-    driverPool[key].driver = new MongoInternals.RemoteCollectionDriver(driverPool[key].connectionString, connectionOptions);
-    driverPool[key].connected = true;
+  if (!Tenants._driverPool[key].driver) {
+    // console.log("connecting!");
+    Tenants._driverPool[key].driver = new MongoInternals.RemoteCollectionDriver(Tenants._driverPool[key].connectionString, Tenants._connectionOptions);
+  }
+
+  if (!Tenants._driverPool[key].driver.mongo.client.topology.isConnected()) {
+    // console.log("reconnecting!");
+    Promise.await(Tenants._driverPool[key].driver.mongo.client.topology.connect());
   }
 
   let collection = context._tenants[key];
   if (!collection) {
+
     // make sure we don't exceed the mongodb collection character size
     collection = new Mongo.Collection(`${context._name}_${tenant.key}`.substr(0, 123), {
-      _driver: driverPool[key].driver
+      _driver: Tenants._driverPool[key].driver
     });
 
     context._tenants[key] = collection;
-    driverPool[key].collections.push(collection);
+    Tenants._driverPool[key].collections.push(collection);
+    Tenants._driverPool[key].collectionNames.push(collection._name);
+
+    collection._tenant = key;
   }
 
   // update the driver pool last accessed date
   // we use for cleanup of driver pool access
-  driverPool[key].lastAccessed = Date.now();
-
-  // if (driverPool[key].connected) {
-  //   driverPool[key].connected = false;
-  //   Promise.await(driverPool[key].driver.mongo.client.close());
-  // }
-  
-  if (!driverPool[key].connected) {
-    driverPool[key].connected = true;
-    Promise.await(driverPool[key].driver.mongo.client.connect());
-  }
+  Tenants._driverPool[key].lastAccessed = Date.now();
 
   return collection;
 }
 
-const connectionHandler = function() {
-  Object.keys(driverPool).forEach(function (key) {
-    const lastAccessed = Date.now() - driverPool[key].lastAccessed;
-    const isConnected = driverPool[key].connected;
-    if (lastAccessed > 2000 && isConnected) {
-      console.log('disconnecting')
-      driverPool[key].driver.mongo.client.close();
-      driverPool[key].connected = false;
-    }
-  })
+// TODO
+// ability to immediately close a connection
+// Mongo.Collection.prototype.close = function () {
+//   const context = this;
+//   context.rawCollection().s.topology.close()
+// }
 
-  Meteor.setTimeout(connectionHandler, 1000);
+const connectionHandler = () => {
+  Object.keys(Tenants._driverPool).forEach((key) => {
+    if (Tenants._driverPool[key].driver) {
+      const isConnected = Tenants._driverPool[key].driver.mongo.client.topology.isConnected();
+      const lastAccessed = Date.now() - Tenants._driverPool[key].lastAccessed;
+      if (isConnected && lastAccessed > Tenants.TENANTS_POOL_DISCONNECT_TIME) {
+        let hasActiveCursor = false;
+        this.Meteor.server.sessions.forEach((session) => {
+          for (const collection of session.collectionViews.keys()) {
+            if (Tenants._driverPool[key].collectionNames.includes(collection)) {
+              hasActiveCursor = true;
+            }
+          }
+        });
+
+        if (hasActiveCursor) {
+          Tenants._driverPool[key].lastAccessed = Date.now();
+        } else {
+          // console.log("disconnecting");
+          Tenants._driverPool[key].driver.mongo.client.topology.close()
+        }
+      }
+    }
+  });
+
+  // run every 10 seconds to cleanout unused connections
+  Meteor.setTimeout(connectionHandler, Tenants.TENANTS_CLEANUP_INTERVAL);
 }
 
-// connectionHandler();
-
-// TODO
-// disconnect from connections
+connectionHandler();
